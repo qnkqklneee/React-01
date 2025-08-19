e1744d877ff953f4fa3853bfd7c28e5a188b8aa4
qnkqklneee <bytemeqklneee@gmail.com>
2025-08-20 02:10:11 +0900

    function-import-export

--- write/main.js
+++ write/main.js
@@ -1,13 +1,48 @@
 //通常の関数
+// function fn(number) {
+//     return number * 2
+// }
+
+// console.log(fn(2));
+
+//アロー関数
+// const fnArrow = (number) => {
+//     return number * 2;
+// }
+
+// console.log(fnArrow(2));
+
+//引数が１つ、return以降の本文が１行の時、簡略化できる
+// const fnArrow = number => {
+//     return number * 2;
+// }
+
+// console.log(fnArrow(2));
+
+// const fnArrowObj = number => ({ result: number * 2})
+
+// // console.log(fnArrow(2));
+// console.log(fnArrowObj(4));
+
+import { hello } from "./module.js";
+import { User } from "./module.js";
+import functionB from "./module.js";//defualtは名前を変えられる
+hello ();//実行
+functionB();
+
+const user = new User("Tom");//classはnewをつけて使用
+user.hello();
+
+//コールバック関数
+function print(callback) {
+    const result = callback(2);
+    console.log(result);
+}
+
 function fn(number) {
     return number * 2
 }
 
-console.log(fn(2));
+debugger;
+print(fn);
 
-//アロー関数
-const fnArrow = (number) => {
-    return number *2;
-}
-
-console.log(fnArrow(2));
