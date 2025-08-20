3e7d6c1714caab3c1e1513ddbbc1a72f3998151b
qnkqklneee <bytemeqklneee@gmail.com>
2025-08-21 01:40:29 +0900

    await/async

--- write/main.js
+++ write/main.js
@@ -5,14 +5,14 @@
 
 // console.log(fn(2));
 
-//アロー関数
+// //アロー関数
 // const fnArrow = (number) => {
 //     return number * 2;
 // }
 
 // console.log(fnArrow(2));
 
-//引数が１つ、return以降の本文が１行の時、簡略化できる
+// //引数が１つ、return以降の本文が１行の時、簡略化できる
 // const fnArrow = number => {
 //     return number * 2;
 // }
@@ -30,19 +30,73 @@ import functionB from "./module.js";//defualtは名前を変えられる
 hello ();//実行
 functionB();
 
-const user = new User("Tom");//classはnewをつけて使用
-user.hello();
+// const user = new User("Tom");//classはnewをつけて使用
+// user.hello();
 
-//コールバック関数
+// //コールバック関数
 function print(callback) {
     const result = callback(2);
     console.log(result);
 }
 
-function fn(number) {
-    return number * 2
-}
+// function fn(number) {
+//     return number * 2
+// }
 
-debugger;
-print(fn);
+// debugger;
+// print(fn);
+
+//0はfalthy、1はtruthy
+// const a = 0;
+// let result = a ? 10 : -10;
+// console.log(result);
+
+const falsy = 0;
+const truthy = 1;
+console.log(Boolean(truthy));
+console.log(Boolean(falsy));
+
+//論理積(&&)
+//&&より左側がfalthyの値ならそれを返す
+//両方がtruthyなら右側を返す
+const resultA = "" && "foo";
+const resultB = NaN && 0;//両方falthyなら左を返す
+const resultC = "foo" && 4;
+
+console.log(resultA);
+console.log(resultB);
+console.log(resultC);
+
+//理論和
+//truthyなら返す、両方truthyなら左を返す
+const resultD = "" || "foo";
+
+console.log(resultD);
+
+//非同期処理
+//同期処理はコードを順番に読み込んでいくこと
+let a = 0;
+
+//resolvea(a)により、2000ミリ秒後1が代入され、
+//それをthen(b)へ代入される
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        a = 1;
+        resolve(a)
+    }, 2000);
+}).then((b) => {
+    console.log(b);
+})
+
+
+//await/asyncで簡略化
+const result = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+        a = 1;
+        resolve(a)
+    }, 4000);
+})
+
+console.log(result);
+
 
