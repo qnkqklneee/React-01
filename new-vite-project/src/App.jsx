47e3ca37862875082e499ac612e676d781c6f0d1
qnkqklneee <akihiro7274@gmail.com>
2025-08-31 02:03:16 +0000

    ipad-test

--- new-vite-project/src/App.jsx
+++ new-vite-project/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import fnArrow from './modules/module'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +19,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>hello world</h1>
+      fnArrow();
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ new-vite-project/src/modules/module.jsx
@@ -0,0 +1,7 @@
+const fnArrow = (number) => {
+    return number * 2;
+};
+
+console.log(fnArrow(7));
+
+export default fnArrow; 
